cc224a7dcae5174801fb97b21af6551f9a630b48
Asmit2952 <asmitbm2952002@gmail.com>
2022-10-21 21:31:47 +0530

    first commit

---
+++ src/components/icons/dev.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const IconDev = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-book-open">
+    <title>Dev</title>
+    <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+  </svg>
+);
+
+export default IconDev;
