71aff05c1daee6bb3e77fe669774ab1bd082eaec
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-08-20 18:40:53 +0530

    revamping stuff

--- src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as useOnClickOutside } from './useOnClickOutside';
-export { default as usePrefersReducedMotion } from './usePrefersReducedMotion';
-export { default as useScrollDirection } from './useScrollDirection';
