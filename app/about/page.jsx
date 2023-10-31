108e8d22ab2292a76c60e5535985aadd441896e5
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-10-31 22:26:06 +0530

    add about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import AnimatedTitle from "@/components/Animations/AnimatedTitle";
+import AnimatedText from "@/components/Animations/AnimatedText";
+import SlidingImages from "@/components/SlidingImages";
+import Image from "next/image";
+import styles from "./page.module.css";
 
-const page = () => {
+export const metadata = {
+    title: "About Asmit Malakannawar",
+};
+
+export default function About() {
     return (
-        <div>page</div>
-    )
+        <div className={styles.main}>
+            <div className={styles.header_container}>
+                <div className={styles.title}>
+                    <h1>
+                        <AnimatedTitle title="Hello." />
+                    </h1>
+                </div>
+                <div className={styles.myphoto}>
+                    <Image fill src="/dp.webp" alt="aboutme image" />
+                </div>
+            </div>
+            <div className={styles.info_text}>
+                <div className={styles.info_text_content}>
+                    <p>
+                        <AnimatedText content="My name is Asmit Malakannawar, and I'm a product designer from India. I am currently a student pursuing computer science. So, why product design?" />
+                        <br />
+                        <br />
+                        <AnimatedText content="Over the years, I found pushing pixels and building information architecture of an app drew my attention other than figuring out the best data structure or the fastest algorithm." />
+                        <br />
+                        <br />
+                        <AnimatedText content="In my spare time, I experiment with designs in Figma. I also help other designers improve their skills by sharing UI & UX related insights. I also love watching new sci-fi shows! Besides that..." />
+                    </p>
+                </div>
+            </div>
+            <div className={styles.activities}>
+                <div className={styles.activities_content}>
+                    <h1>
+                        <AnimatedTitle title="I love speaking at conferences, travelling and photography!" />
+                    </h1>
+                </div>
+                <div className={styles.sliding_images}>
+                    <SlidingImages />
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default page
