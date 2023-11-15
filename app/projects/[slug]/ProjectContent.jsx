a02434e8fdbe3155d75712dfab6a53f9b640a64b
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-11-15 12:46:06 +0530

    remove text-justify

--- app/projects/[slug]/ProjectContent.jsx
+++ app/projects/[slug]/ProjectContent.jsx
@@ -98,7 +98,7 @@ export default function ProjectContent({ project }) {
                         </div>
                     </div>
                 </div>
-                <article className="prose prose-lg mx-auto text-justify">
+                <article className="prose prose-lg mx-auto">
                     <MDXContent />
                 </article>
             </div>
