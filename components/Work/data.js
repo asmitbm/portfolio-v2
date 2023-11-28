40e235200461beacc7c75724816eba63ff591850
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-11-29 00:24:45 +0530

    update order

--- components/Work/data.js
+++ components/Work/data.js
@@ -1,5 +1,14 @@
 const projects = [
     {
+        title: "Ubuntu with Spatial UI",
+        src: "/projects/ubuntuui.webp",
+        description: "OS design using Spatial UI",
+        period: "Concept Design",
+        link: "/projects/ubuntu-spatial-ui-design",
+        color: "#7C2D00",
+        bgcolor: "#FDF5EF",
+    },
+    {
         title: "conda-forge",
         src: "/projects/condaforge.webp",
         description: "Website redesign focusing on accessibility",
@@ -16,15 +25,6 @@ const projects = [
         link: "/projects/oras-website",
         color: "#00397C",
         bgcolor: "#EFF5FD",
-    },
-    {
-        title: "Ubuntu with Spatial UI",
-        src: "/projects/ubuntuui.webp",
-        description: "OS design using Spatial UI",
-        period: "Concept Design",
-        link: "/projects/ubuntu-spatial-ui-design",
-        color: "#7C2D00",
-        bgcolor: "#FDF5EF",
     },
 ];
 
