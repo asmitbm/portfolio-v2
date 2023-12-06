4dd5183be082970297a275394e008736ea356818
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-12-06 08:22:12 +0530

    edge runtime fix

--- app/api/og/route.jsx
+++ app/api/og/route.jsx
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { ImageResponse } from "next/server";
 
 export const runtime = "edge";
 
@@ -6,25 +6,17 @@ export async function GET() {
     const imageData = await fetch(new URL("./og.png", import.meta.url)).then(
         (res) => res.arrayBuffer()
     );
-    return new NextRequest(
+    return new ImageResponse(
         (
             <div
                 style={{
                     display: "flex",
-                    background: "#f6f6f6",
-                    width: "100%",
-                    height: "100%",
                     flexDirection: "column",
                     justifyContent: "center",
                     alignItems: "center",
                 }}
             >
-                <img
-                    width="1200"
-                    height="630"
-                    object-fit="fill"
-                    src={imageData}
-                />
+                <img width="1200" height="630" src={imageData} />
             </div>
         ),
         {
