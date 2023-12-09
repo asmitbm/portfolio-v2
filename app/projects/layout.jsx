b35b6a28992f687a0a4f7fac2ed4292766616f42
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-12-09 19:18:27 +0530

    fix projects page

--- app/projects/layout.jsx
+++
@@ -1,9 +0,0 @@
-import Project from "./page";
-
-export const metadata = {
-    title: "Projects by Asmit Malakannawar",
-    description:
-        "See what Asmit has been working on! Explore Asmit's projects and case studies to see the creative solutions that he has crafted.",
-};
-
-export default Project;
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,14 +1,15 @@
-"use client";
-import { useEffect } from "react";
 import Projects from "@/components/Projects";
 import AnimatedTitle from "@/components/Animations/AnimatedTitle";
 import AnimatedText from "@/components/Animations/AnimatedText";
 import styles from "./page.module.css";
 
+export const metadata = {
+    title: "Projects by Asmit Malakannawar",
+    description:
+        "See what Asmit has been working on! Explore Asmit's projects and case studies to see the creative solutions that he has crafted.",
+};
+
 export default function Project() {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
     return (
         <div className={styles.background_image}>
             <div className={styles.main}>
